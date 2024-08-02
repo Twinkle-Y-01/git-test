5cd61c679903552c6e2861b3377f6cd3bd52f020
Y <Y@qq.com>
2024-08-02 17:49:46 +0800

    push test

--- src/main/java/GItTest.java
+++ src/main/java/GItTest.java
@@ -3,5 +3,6 @@ public class GItTest {
         System.out.println("git test 1");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push test");
     }
 }
