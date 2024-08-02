ee58a04423682422eca5c3e5330f2a3c00fa2faa
Y <Y@qq.com>
2024-08-02 18:22:18 +0800

    ssh push

--- src/main/java/GItTest.java
+++ src/main/java/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
         System.out.println("master commit");
         System.out.println("hot-fix commit");
         System.out.println("push test");
+        System.out.println("ssh push");
     }
 }
